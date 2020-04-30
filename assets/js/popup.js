45be44cfd1cd4f509bb52bf743b917ecd37b02ef
AlphabetClub <57715128+AlphabetClub@users.noreply.github.com>
2020-04-30 21:38:29 +0530

    update

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,7 +1,4 @@
 $(document).ready(function(){  
-      if(localStorage.getItem('popState') != 'shown'){
-        $("#myModal").delay(5000).fadeIn();
-        localStorage.setItem('popState','shown')
-    }
+     
    $('#myModal').modal('show');
     });
